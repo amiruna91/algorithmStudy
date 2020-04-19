75b1af3325747d984a43309ecfe497e858d265dc
jooyoung <amiruna91@gmail.com>
2020-04-19 20:49:18 +0900

    2020-04-19 알고리즘 추가

---
+++ programmers/src/Level 1/30-하샤드 수.java
@@ -0,0 +1,33 @@
+package programmers;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		/*
+		 * 하샤드 수
+		 * 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 
+		 * 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 
+		 * 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+		 * 
+		 * arr	return
+			10	true
+			12	true
+			11	false
+			13	false
+		 */
+		
+		int x = 11;
+		boolean answer = false;
+		int total = 0;
+		int y = x;
+		
+		while (y > 0) {
+			int num = y%10;
+			total += num;
+			y = y/10;
+		}
+		System.out.println(total);
+		answer = (x%total == 0) ? true : false;
+		System.out.println(answer);
+	}
+}
